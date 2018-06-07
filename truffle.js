199339e2fb001da0ca6fc95aa4906adb3ecf4cc9
Mert Ozdag <mertozdag@Merts-MacBook-Pro.local>
2018-06-07 11:11:11 -0400

    modified for kaleido

--- truffle.js
+++ truffle.js
@@ -1,9 +1,14 @@
+var Web3 = require('web3')
+
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+      provider: () => {
+        return new Web3.provider.HttpProvider('https://u0s936cp08-u0m0medsuq-rpc.us-east-2.kaleido.io/',0,'u0etp2eajs','NJTD8hlMRDrAKkKJzgJf17pbxYrFVPINOvVLhhL5A9Q')
+      },
+      network_id: "*",
+      gasPrice: 0,
+      gas: 4500000      
     }
   }
 };
