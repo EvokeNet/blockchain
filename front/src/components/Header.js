219c63a39eee5b63f63391730ea0abdcf79fca88
Kel Kanhirun <kel.kanhirun@gmail.com>
2018-06-19 21:39:00 -0400

    Adds tests for Header component

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -5,16 +5,24 @@ import donorIcon from "../assets/img/donor-icon.png"
 import "./Header.css"
 
 class Header extends Component {
+
+  formatEvocoin(amount) {
+    const formattedValue = Number(this.props.balance).toLocaleString() + '.0'
+    const tickerSymbol = 'EVC';
+
+    return `${ formattedValue } ${ tickerSymbol }`
+  }
+
   render() {
     return (
       <header className="App">
         <div className="App__Row">
             <div className="App__Row1">
                 <img src={ evokeLogo } alt="The Evoke logo."/>
-                <h1 className="App__DonorName">Name / organization</h1>
+                <h1 className="App__DonorName" data-donor-name>{ this.props.name }</h1>
                 <img src={ donorIcon } alt="A profile of the organization."/>
             </div>
-            <h1 className="App__DonorBalance">52,000.0 EVC</h1>
+            <h1 className="App__DonorBalance" data-donor-balance>{ this.formatEvocoin(this.props.balance) }</h1>
         </div>
       </header>
     );
--- front/src/test/Header.test.js
+++ front/src/test/Header.test.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
 import Header from '../components/Header.js'
 
 describe("Header", () => {
@@ -7,8 +10,32 @@ describe("Header", () => {
   });
 
   describe("when Donor has EVC", () => {
-    it("shows the Donor's organization", () => {});
-    it("shows the Donor's balance", () => {});
+    it("shows the Donor's organization", () => {
+      const subject = shallow(<Header name="aDonor" balance={ 1000 }/>);
+
+      const aNode = subject.find("[data-donor-name]");
+
+      expect(aNode.exists()).toEqual(true);
+      expect(aNode.text()).toEqual("aDonor");
+    });
+
+    it("shows the Donor's balance for a small amount", () => {
+      const subject = shallow(<Header name="aDonor" balance={ 1000 }/>);
+
+      const aNode = subject.find("[data-donor-balance]");
+
+      expect(aNode.exists()).toEqual(true);
+      expect(aNode.text()).toEqual("1,000.0 EVC");
+    });
+
+    it("shows the Donor's balance for a big amount", () => {
+      const subject = shallow(<Header name="aDonor" balance={ 999888 }/>);
+
+      const aNode = subject.find("[data-donor-balance]");
+
+      expect(aNode.exists()).toEqual(true);
+      expect(aNode.text()).toEqual("999,888.0 EVC");
+    });
   });
 
 });
